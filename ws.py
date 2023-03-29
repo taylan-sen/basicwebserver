46c9cbb1c3f02bba65896546c1407b8c96dbf4ab
Taylan Sen <tsen@niagara.edu>
2023-03-29 16:52:15 +0000

    Added code to accept incoming connections.

--- ws.py
+++ ws.py
@@ -1,13 +1,13 @@
 # This program implements a basic webserver.
+import socket
 
 print('Webserver starting')
 
-
-
-
-
-
-
-
+with socket.socket() as s:
+  s.bind( ('0.0.0.0', 14000) )
+  s.listen()
+  conn,addr = s.accept()
+  with conn:
+    print('Incoming connection from:', addr)
 
 print('Webserver complete')
